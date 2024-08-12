c45a08b6b9e47369f2f36c5c7a79f998c57973eb
5160369 <5160369@liae.edu.uniube.br>
2024-08-12 20:40:08 -0300

    qualquer

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-echo "Olá mundo";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="gravar.php" method="post">
+        <input type="text" name="Nome">
+        <input type="submit">
+    </form>
+</body>
+</html>
